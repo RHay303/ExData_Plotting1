eab49aa73539302ef42a1ddc55f02ac3aa810c2c
Rebecca Hay <rp303@yahoo.com>
2021-02-13 13:00:57 -0700

    Plots and related R files

---
+++ plot1.R
@@ -0,0 +1,87 @@
+## ----setup, include=FALSE-------------------------------------------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+library(R.utils)
+library(dplyr)
+library(lubridate)
+
+
+## ----getdataset-----------------------------------------------------------------------------------------------
+#set working directory
+
+setwd("C:/Users/rp303/OneDrive/Documents/coursera data science/ExData_Plotting1")
+
+#create file
+
+filename <- "exdata_data_household_power_consumption.zip"
+
+## Check if the file exists already in the directory and if not, download and unzip the files in a folder
+##named ExData_Plotting1
+
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip "
+  download.file(fileURL, filename, method="curl")
+}  
+if (!file.exists("exdata_data_household_power_consumption")) { 
+  unzip(filename) 
+}
+
+
+
+## ----calcmemoryrequired---------------------------------------------------------------------------------------
+# total lines on a file 
+sourcefile <- "household_power_consumption.txt"
+numrows <- countLines(sourcefile);
+#read header row to determine number of columns
+dat <- read.table(sourcefile, nrows = 1, header =TRUE, sep =';')[-1, ]
+numcolumns=ncol(dat)
+
+#calculate the memory required to read file before loading it
+#memory required = no. of column * no. of rows * 8 bytes/numeric
+#1 MB=1000 bytes
+
+#bytes
+memoryrequired=numcolumns*numrows*8 
+
+#megabytes converted to bytes
+memoryavailable=memory.size()  *1000
+
+if (memoryrequired<=memoryavailable) {
+  fileTooLarge= TRUE 
+  } else {
+    fileTooLarge=FALSE
+  }
+
+
+
+## -------------------------------------------------------------------------------------------------------------
+if (!fileTooLarge) {
+  df1<-read.table(sourcefile,header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+  
+  #convert Date field from chr to date
+  df1$Date<- as.Date(df1$Date, "%d/%m/%Y")
+  
+  #filter data to dates of interest
+  df2<-df1%>%select(everything())%>%
+    filter(between(Date, as.Date("2007-02-01"),as.Date("2007-02-02")))
+}  
+  
+
+
+## -------------------------------------------------------------------------------------------------------------
+#convert Global_active_power from chr to num
+df2$Global_active_power=as.numeric(df2$Global_active_power)
+
+getwd()
+
+#dev.cur()
+
+#define destinatio for png
+png("Plot1.png",width=480, height=480)
+
+#Create histogram
+hist(df2$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts")
+
+ 
+dev.off()
+
+
